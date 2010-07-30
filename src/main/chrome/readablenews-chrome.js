4eb051e922bd833387af8e281b72bfc08bd63a78
unknown <mingfai@.(none)>
2010-07-31 03:38:49 +0800

    updated

--- src/main/chrome/readablenews-chrome.js
+++ src/main/chrome/readablenews-chrome.js
@@ -1,32 +1,30 @@
 try{
 
-  var css = function(cssPath){
-    var cssUrl = chrome.extension.getURL(cssPath) + '?r=' + Math.random();
-    $('head').append('<link rel="stylesheet" href="' + cssUrl + '" type="text/css" />');
+  var css = function(path){
+    var link = document.createElement("link")
+    link.href = path
+    link.type = "text/css"
+    link.rel = "stylesheet"
+    document.getElementsByTagName("head")[0].appendChild(link);
+  }
+
+  var js = function(path){
+    /*var script = document.createElement("script")
+     script.id = 'readablenews-loader'
+     script.src = chrome.extension.getURL("js/loader.js") + '?js=' + chrome.extension.getURL("js/hk/hk.news.yahoo.com.js");
+     document.getElementsByTagName("head")[0].appendChild(script);*/
   }
 
   var url = location.href
   if (url.search(/hk\.news\.yahoo\.com/) > 0){
     //css
-    css("hk.news.yahoo.com/readablenews-home.css")
-    css("hk.news.yahoo.com/readablenews-site.css")
-
-
-    //js
-    /*var script = document.createElement("script")
-    script.id = 'readablenews-loader'
-    script.src = chrome.extension.getURL("js/loader.js") + '?js=' + chrome.extension.getURL("js/hk/hk.news.yahoo.com.js");
-    document.getElementsByTagName("head")[0].appendChild(script);*/
-
+    css(chrome.extension.getURL("hk.news.yahoo.com/readablenews-home.css") + '?r=' + Math.random())
+    css(chrome.extension.getURL("hk.news.yahoo.com/readablenews-site.css") + '?r=' + Math.random())
+    /*css("hk.news.yahoo.com/readablenews-site.css")*/
   } else if (location.href.search(/hk\.apple\.nextmedia\.com/) > 0){
-    // css
-    writeCss("css/hk/hk.apple.nextmedia.com.css")
+    css(chrome.extension.getURL("hk.apple.nextmedia.com/readablenews-home.css") + '?r=' + Math.random())
+    css(chrome.extension.getURL("hk.apple.nextmedia.com/readablenews-site.css") + '?r=' + Math.random())
 
-    //js
-    var script = document.createElement("script")
-    script.id = 'readablenews-loader'
-    script.src = chrome.extension.getURL("js/loader.js") + '?js=' + chrome.extension.getURL("js/hk/hk.apple.nextmedia.com.js");
-    document.getElementsByTagName("head")[0].appendChild(script);
 
   }
 
--- src/main/firefox/content/readablenews-firefox.js
+++ src/main/firefox/content/readablenews-firefox.js
@@ -4,32 +4,46 @@ window.addEventListener("load", function(){
 
       if (typeof(Firebug) !== undefined) console = Firebug.Console;
 
-      var loadJQuery = function(){
-        jQuery.noConflict();
-        $ = function(selector, context){ return new jQuery.fn.init(selector, context || evt.originalTarget); };
-        $.fn = $.prototype = jQuery.fn;
-        console.log('loaded jQuery')
+      /*var loadJQuery = function(){
+       jQuery.noConflict();
+       $ = function(selector, context){ return new jQuery.fn.init(selector, context || evt.originalTarget); };
+       $.fn = $.prototype = jQuery.fn;
+       console.log('loaded jQuery')
+       }*/
+
+      var css = function(path){
+        var link = evt.originalTarget.createElement("link")
+        link.href = path
+        link.type = "text/css"
+        link.rel = "stylesheet"
+        evt.originalTarget.getElementsByTagName("head")[0].appendChild(link);
       }
 
       try{
         var url = evt.originalTarget.location.href
         if (url.search(/hk\.news\.yahoo\.com/) > 0){
-          loadJQuery();
           console.log('hk.news.yahoo.com')
-          $('head').append('<link rel="stylesheet" href="resource://hk.news.yahoo.com-readablenews-home.css" type="text/css" />');
-          $('head').append('<link rel="stylesheet" href="resource://hk.news.yahoo.com-readablenews-site.css" type="text/css" />');
-        }else if (url.search(/hk\.apple\.nextmedia\.com/) > 0){
-          loadJQuery();
+          css("resource://hk.news.yahoo.com-readablenews-home.css")
+          css("resource://hk.news.yahoo.com-readablenews-site.css")
+          /*$('head').append('<link rel="stylesheet" href="resource://hk.news.yahoo.com-readablenews-home.css" type="text/css" />');
+           $('head').append('<link rel="stylesheet" href="resource://hk.news.yahoo.com-readablenews-site.css" type="text/css" />');*/
+        } else if (url.search(/hk\.apple\.nextmedia\.com/) > 0){
           console.log('hk.apple.nextmedia.com')
-          $('head').append('<link rel="stylesheet" href="resource://hk.apple.nextmedia.com-readablenews-home.css" type="text/css" />');
-          $('head').append('<link rel="stylesheet" href="resource://hk.apple.nextmedia.com-readablenews-site.css" type="text/css" />');
+          css("resource://hk.apple.nextmedia.com-readablenews-home.css")
+          css("resource://hk.apple.nextmedia.com-readablenews-site.css")
         }
       } catch(e){
         console.log('error: ' + e)
       }
-    }, true);
+    }, true
+            )
+            ;
   }
-}, false);
+}
+        ,
+        false
+        )
+        ;
 /*
  try{
  var css = function(cssPath){
